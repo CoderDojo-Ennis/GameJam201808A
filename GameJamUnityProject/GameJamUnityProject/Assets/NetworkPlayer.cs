7819cfb16d510ae9a7f38bb675975d3581219585
Coder Dojo Ennis VR5 <enniscoderdojo@gmail.com>
2018-08-18 15:03:42 +0100

    Network lobby scene and russ test scene

---
+++ GameJamUnityProject/GameJamUnityProject/Assets/NetworkPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class NetworkPlayer : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+}
