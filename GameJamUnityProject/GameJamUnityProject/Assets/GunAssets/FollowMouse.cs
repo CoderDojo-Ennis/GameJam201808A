6bc6d95e7de97aa46a9fd925c5627684bc42406a
Joey Corbett <joeyecorbett@gmail.com>
2018-08-18 15:38:24 +0100

    gun

---
+++ GameJamUnityProject/GameJamUnityProject/Assets/GunAssets/FollowMouse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour {
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float H = 0.0f;
+    private float V = 0.0f;
+    // Use this for initialization
+    void Start () {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        //if(Input.GetAxis("Cancel")) 
+	}
+	
+	// Update is called once per fram
+
+    void Update()
+    {
+        H += speedH * Input.GetAxis("Mouse X");
+        V -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(V, H, 0.0f);
+    }
+}
+
